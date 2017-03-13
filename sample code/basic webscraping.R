1bbf01cea3ca7b3544caba5befd3b9604b960870
Jianming Zhou <jz1584@nyu>
2017-03-13 15:13:55 -0400

    frame

--- sample code/basic webscraping.R
+++ sample code/basic webscraping.R
@@ -4,14 +4,33 @@ library(rvest)
 
 #Extract properties price table data from trulia 
 
+# 
+# kingsUrl<-read_html("https://www.trulia.com/home_prices/New_York/Kings_County-heat_map/")
+# queensUrl<-read_html("https://www.trulia.com/home_prices/New_York/Queens_County-heat_map/")
+# nycUrl<-read_html('https://www.trulia.com/home_prices/New_York/New_York_County-heat_map/')
+# bronxUrl<-read_html('https://www.trulia.com/home_prices/New_York/Bronx_County-heat_map/')
+# statenislandUrl<-read_html('https://www.trulia.com/home_prices/New_York/Richmond_County-heat_map/')
 
-url<-read_html("https://www.trulia.com/home_prices/New_York/Kings_County-heat_map/")
 
-HomePrice<-url %>% html_nodes(xpath = '//*[@id="heatmap_table"]/table')%>%
-  html_table()
+HomePrice<-queensUrl %>% html_nodes(xpath = '//*[@id="heatmap_table"]/table') %>% html_table()
 
 HomePrice<-HomePrice[[1]]
 names(HomePrice)<-HomePrice[1,]
 HomePrice[-c(1:2),]
+
+
+for (i in c('Kings_County','Queens_County','New_York_County','Bronx_County','Richmond_County')){
+  url<-paste0("https://www.trulia.com/home_prices/New_York/",i,'-heat_map/')
+  rhtml<-read_html(url)
+  HomePrice<-rhtml%>% html_nodes(xpath = '//*[@id="heatmap_table"]/table') %>% html_table()
+  HomePrice<-HomePrice[[1]]
+  names(HomePrice)<-HomePrice[1,]
+  HomePrice<-HomePrice[-c(1:2),]
+  print('-------------------------------------Stop Line\n')
+  write.csv(HomePrice,file = paste('data/',i,'(',names(HomePrice)[3],').csv'))
+}
+
+
+
 
 
